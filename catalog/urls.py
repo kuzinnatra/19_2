bdb07f8a05d69992542cf0a54bb798b0ea9e3064
kuzin_sn <kuzinnatra@yandex.ru>
2024-09-15 19:17:39 +0400

    Реализована страница продукта (пустая)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import products_list, contacts
+from catalog.views import products_list, contacts, product_detail
 
 urlpatterns = [
     path('', products_list),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('product/<int:pk>', product_detail, name='product_detail' )
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 import catalog
@@ -11,7 +11,14 @@ import catalog
 def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'catalog/base.html', context)
+    return render(request, 'catalog/products_list.html', context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html', context)
+
+
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
